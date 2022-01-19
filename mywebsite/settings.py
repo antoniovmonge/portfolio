f69682ce92a8d048673faa5c3da25dea1613ea56
Antonio Vilchez Monge <antoniovmonge@gmail.com>
2022-01-19 17:09:08 +0100

    ALLOWED_HOSTS

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost', '127.0.0.1', 'portfolio-antonio-vm.herokuapp.com'
+]
 
 # Application definition
 
