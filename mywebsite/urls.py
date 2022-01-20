cd9f095039d49bdb83655877913f0e5cb88ea483
Antonio Vilchez Monge <antoniovmonge@gmail.com>
2022-01-20 10:19:01 +0100

    favicon + project description

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -15,5 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
-urlpatterns = [path('admin/', admin.site.urls), path('', include('base.urls'))]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('base.urls')),
+    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),),
+]
